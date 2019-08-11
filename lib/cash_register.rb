17411d66ad5d4f1cf2225eb5050cbf5854253e0e
Christopher Baptiste <chrisbaptiste83@gmail.com>
2019-08-11 08:10:40 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 class CashRegister 
   attr_accessor :total, :discount, :price, :quantity, :items
-  def initialize(discount=nil) 
+  def initialize(discount=0) 
     @total = 0 
     @discount = discount
    @items =[]
@@ -18,7 +18,7 @@ class CashRegister
 end 
 
   def apply_discount 
-    if discount 
+    if discount > 0
   @total = @total - (@discount/100.to_f * @total).to_i
    "After the discount, the total comes to $#{@total}." 
 else 
